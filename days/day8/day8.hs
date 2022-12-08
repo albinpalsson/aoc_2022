107a81780b5e18b1055ac2b540b90d48f0f13f54
Albin Palsson <albin_palsson@hotmail.se>
2022-12-08 09:29:02 +0100

    day8

---
+++ days/day8/day8.hs
@@ -0,0 +1,102 @@
+module Main where
+
+import AocShared (expect, ftrace, readLines)
+import Data.Char
+import Data.Maybe
+
+main :: IO ()
+main = do
+  lines <- readLines "input.txt"
+  print (parse example)
+  expect (tallest (Coord 3 3) up (parse example)) 7
+  expect (isVis (Coord 1 1) (parse example)) True
+  expect (isVis (Coord 2 2) (parse example)) False
+  expect (countVis (Coord 0 0) 0 (parse example)) 21
+  expect (part1 lines) 1803
+  expect (scenicScore (Coord 2 1) (parse example)) 4
+  expect (scenicScore (Coord 2 3) (parse example)) 8
+  expect (maxScenic (Coord 0 0) 0 (parse example)) 8
+  expect (part2 lines) 268912
+
+example = ["30373", "25512", "65332", "33549", "35390"]
+
+data Coord = Coord {x :: Int, y :: Int} deriving (Show)
+
+type Grid = [[Int]]
+
+type Dir = (Coord -> Coord)
+
+parse :: [[Char]] -> Grid
+parse = map (map digitToInt)
+
+size :: Grid -> Coord
+size g = Coord (length (g !! 0)) (length g)
+
+outside :: Coord -> Grid -> Bool
+outside (Coord x y) g = or [x < 0, x >= mx, y < 0, y >= my]
+  where
+    (Coord mx my) = size g
+
+isVis :: Coord -> Grid -> Bool
+isVis coord g = any visInDir [up, down, left, right]
+  where
+    visInDir dir = h > (tallest coord dir g)
+    h = g `at` coord
+
+right, left, up, down :: Dir
+right (Coord x y) = Coord (x + 1) y
+left (Coord x y) = Coord (x - 1) y
+up (Coord x y) = Coord x (y - 1)
+down (Coord x y) = Coord x (y + 1)
+
+at :: [[a]] -> Coord -> a
+at g c@(Coord x y) = (g !! y) !! x
+
+tallest :: Coord -> Dir -> Grid -> Int
+tallest pos@(Coord x y) dir g
+  | next `outside` g = -1
+  | otherwise = max (g `at` next) (tallest next dir g)
+  where
+    next = dir pos
+
+countVis :: Coord -> Int -> Grid -> Int
+countVis coord acc g
+  | isJust next = countVis (fromJust next) acc' g
+  | otherwise = acc'
+  where
+    next = iterNext coord g
+    acc' = if isVis coord g then acc + 1 else acc
+
+viewingDist :: Coord -> Int -> Dir -> Grid -> Int
+viewingDist pos h dir g
+  | next `outside` g = 0
+  | (g `at` next) < h = (viewingDist next h dir g) + 1
+  | otherwise = 1
+  where
+    next = dir pos
+
+scenicScore :: Coord -> Grid -> Int
+scenicScore coord g = product $ map vd [up, down, left, right]
+  where
+    vd dir = viewingDist coord h dir g
+    h = g `at` coord
+
+maxScenic :: Coord -> Int -> Grid -> Int
+maxScenic c best g
+  | isJust next = maxScenic (fromJust next) best' g
+  | otherwise = best'
+  where
+    next = iterNext c g
+    best' = max best (scenicScore c g)
+
+iterNext :: Coord -> Grid -> Maybe Coord
+iterNext c@(Coord x y) g
+  | x < mx - 1 = Just (right c)
+  | y < my - 1 = Just (Coord 0 (y + 1))
+  | otherwise = Nothing
+  where
+    (Coord mx my) = size g
+
+part1, part2 :: [[Char]] -> Int
+part1 = (countVis (Coord 0 0) 0) . parse
+part2 = (maxScenic (Coord 0 0) 0) . parse
